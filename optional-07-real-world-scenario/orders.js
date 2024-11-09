279c0f8c5b83f031227302302748cf14a49370ae
Alif Bayu Ammarizky <148053752+fearless2905@users.noreply.github.com>
2024-11-09 13:55:40 +0700

    Add files via upload

---
+++ optional-07-real-world-scenario/orders.js
@@ -0,0 +1,48 @@
+// Gunakan fungsi di bawah ini untuk menghasilkan id yang unik
+function generateUniqueId() {
+  return `_${Math.random().toString(36).slice(2, 9)}`;
+}
+
+// Variabel yang menampung data orders
+let orders = [];
+
+// Fungsi untuk menambah order
+function addOrder(customerName, items) {
+  const totalPrice = items.reduce((total, item) => total + item.price, 0);
+  const newOrder = {
+    id: generateUniqueId(),
+    customerName,
+    items,
+    totalPrice,
+    status: "Menunggu",
+  };
+  orders.push(newOrder);
+}
+
+// Fungsi untuk memperbarui status order
+function updateOrderStatus(orderId, status) {
+  const order = orders.find((order) => order.id === orderId);
+  if (order) {
+    order.status = status;
+  }
+}
+
+// Fungsi untuk menghitung total revenue dari order yang berstatus Selesai
+function calculateTotalRevenue() {
+  return orders
+    .filter((order) => order.status === "Selesai")
+    .reduce((total, order) => total + order.totalPrice, 0);
+}
+
+// Fungsi untuk menghapus order
+function deleteOrder(id) {
+  orders = orders.filter((order) => order.id !== id);
+}
+
+export {
+  orders,
+  addOrder,
+  updateOrderStatus,
+  calculateTotalRevenue,
+  deleteOrder,
+};
